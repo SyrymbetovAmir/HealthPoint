c37a8671da18860f9ff93d13eb5f2d2ea37d7ba2
Dekei77 <153479497+Dekei77@users.noreply.github.com>
2025-02-06 16:48:41 +0500

    Update and rename manageaccount.js to manageacc.js

--- manageacc.js
+++ manageacc.js
@@ -1,3 +1,16 @@
+const body = document.querySelector("body"),
+      modeToggle = body.querySelector(".mode-toggle");
+      sidebar = body.querySelector("nav");
+      sidebarToggle = body.querySelector(".sidebar-toggle");
+
+modeToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+
+});
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+
+})
 document.addEventListener("DOMContentLoaded", () => {
     const reloadButton = document.querySelector(".reload");
     const tableBody = document.querySelector(".table");
